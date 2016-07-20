17a40fbdecfb2a1055a2d0776b5726e736be0869
Edward Yoo <ehyoo97@gmail.com>
2016-07-19 23:27:26 -0400

    beginning to implement favourites

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ require 'bcrypt'
 
 class User < ActiveRecord::Base
   has_many :pips
+  has_many :favorites
+  has_many :favorited_pips, through: :favorites, source: :favorite
   has_secure_password # THIS IS MAGIC.
 end
