cee3735f79823a09225bde91b8c0c7400cb820c6
Edward Yoo <ehyoo97@gmail.com>
2016-07-23 18:28:48 -0400

    user can favorite pips

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,8 +1,18 @@
 class FavoritesController < ApplicationController
   # you can create or destroy a favorite.
   def create
+    @favorite = Favorite.new(user: current_user, pip_id: params[:pip_id])
+    if @favorite.save
+      redirect_to root_url
+    end
+    # TODO: Add some kind of flashing.
   end
 
   def destroy
+    Favorite.where(user_id: current_user.id, pip_id: params[:pip_id]).first.destroy
+    respond_to do |format|
+      format.html { redirect_to root_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ require 'bcrypt'
 class User < ActiveRecord::Base
   has_many :pips
   has_many :favorites
-  has_many :favorited_pips, through: :favorites, source: :favorite
+  has_many :favorited_pips, through: :favorites, source: :pip
   has_secure_password # THIS IS MAGIC.
 end
